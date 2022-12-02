104d4db826a4d62f2d4744458dc88a801723609e
lamongabriel <gabriel-lamon@outlook.com>
2022-12-02 14:47:14 -0300

    authetication middleware

---
+++ api/src/middlewares/withAuth.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import User from '../models/User'
+
+dotenv.config()
+
+interface TokenPayload {
+  _id: string
+  email: string
+  iat: number
+  exp: number
+}
+
+export async function withAuth (req: Request, res: Response, next: NextFunction) {
+  try {
+    const { authorization } = req.headers
+
+    if (!authorization) {
+      return res.sendStatus(403)
+    }
+
+    const token = authorization.replace('Bearer', '').trim()
+    const payload = jwt.verify(token, process.env.SECRET as string) as TokenPayload
+
+    const { _id } = payload
+    const user = await User.findById(_id)
+
+    if (user) {
+      req.user = user
+    } else {
+      return res.sendStatus(500)
+    }
+
+    next()
+  } catch {
+    return res.sendStatus(403)
+  }
+}
--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -3,9 +3,11 @@ import { Router } from 'express'
 import { registerUser } from '../useCases/user/registerUser'
 import { loginUser } from '../useCases/user/loginUser'
 
+import { withAuth } from '../middlewares/withAuth'
+
 const routes = Router()
 
 routes.post('/register', registerUser)
-routes.post('/login', loginUser)
+routes.post('/login', withAuth, loginUser)
 
 export default routes
