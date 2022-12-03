dc8c879bcf9377f75c72fc6930f33d16232b94c2
lamongabriel <gabriel-lamon@outlook.com>
2022-12-02 23:29:51 -0300

    removed auth middleware at login route

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -3,11 +3,9 @@ import { Router } from 'express'
 import { registerUser } from '../useCases/user/registerUser'
 import { loginUser } from '../useCases/user/loginUser'
 
-import { withAuth } from '../middlewares/withAuth'
-
 const routes = Router()
 
 routes.post('/register', registerUser)
-routes.post('/login', withAuth, loginUser)
+routes.post('/login', loginUser)
 
 export default routes
