8b97a30045414db44fba9603d64cfe4a3a5e3798
lamongabriel <gabriel-lamon@outlook.com>
2022-12-02 23:20:23 -0300

    update note route

--- api/src/routes/notes.routes.ts
+++ api/src/routes/notes.routes.ts
@@ -5,11 +5,15 @@ import { withAuth } from '../middlewares/withAuth'
 import { createNote } from '../useCases/note/createNote'
 import { listNotesByUserId } from '../useCases/note/listNotesByUserId'
 import { listNoteById } from '../useCases/note/listNoteById'
+import { updateNote } from '../useCases/note/updateNote'
 
 const routes = Router()
 
-routes.post('/notes', withAuth, createNote)
 routes.get('/notes', withAuth, listNotesByUserId)
 routes.get('/notes/:id', withAuth, listNoteById)
+
+routes.post('/notes', withAuth, createNote)
+
+routes.put('/notes/:id', withAuth, updateNote)
 
 export default routes
---
+++ api/src/useCases/note/updateNote.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express'
+
+import Note from '../../models/Note'
+
+export async function updateNote (req: Request, res: Response) {
+  try {
+    const { title, body } = req.body
+    const { id } = req.params
+
+    if (!title || !body || !id) {
+      return res.status(400).json({
+        message: 'Invalid data, field TITLE and BODY are REQUIRED.'
+      })
+    }
+
+    const note = await Note.findById(id)
+    if (!note) {
+      return res.status(400).json({
+        message: 'Could not find a note with this ID.'
+      })
+    }
+
+    if (String(note.author) === String(req.user._id)) {
+      await note.updateOne({
+        $set: {
+          title,
+          body
+        }
+      }, {
+        new: true,
+        upsert: true
+      })
+      return res.status(200).json({ id, title, body })
+    } else {
+      return res.status(403).json({
+        message: 'You cannot update a note that is not yours.'
+      })
+    }
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+}
