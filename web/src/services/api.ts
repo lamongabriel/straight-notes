4d137763a4ae120a47a78b3b126cc4cdb55fc1d5
lamongabriel <gabriel-lamon@outlook.com>
2022-12-07 01:20:17 -0300

    removed interceptors from axios api

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -3,9 +3,3 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'http://localhost:3001'
 })
-
-api.interceptors.response.use(response => response, error => {
-  if (error.response.status === 403) {
-    localStorage.removeItem('straightnotes@token')
-  }
-})
