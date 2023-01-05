e6dd6e14514a8c9d4a146ed87a76d8423eda0151
lamongabriel <gabriel-lamon@outlook.com>
2023-01-05 14:04:46 -0300

    better search

--- web/src/components/HeaderLogged/Search.tsx
+++ web/src/components/HeaderLogged/Search.tsx
@@ -1,58 +1,86 @@
 import { ChangeEvent, useState } from 'react'
+import { NotesServices } from '../../services/notes'
 
 import { useNotes } from '../../hooks/useNotes'
 
-import { FormControl, FormLabel, InputGroup, Input, InputRightElement, Button } from '@chakra-ui/react'
+import { FormControl, FormLabel, InputGroup, Input, InputRightElement, Button, Box } from '@chakra-ui/react'
 
 import { X } from 'phosphor-react'
 
-import { NotesServices } from '../../services/notes'
-
 export function Search () {
   const [query, setQuery] = useState('')
-  const { setNotes } = useNotes()
+  const [isLoading, setIsLoading] = useState(false)
+  const { notes, setNotes, setCurrentNote } = useNotes()
 
   function handleChange (e: ChangeEvent<HTMLInputElement>) {
     setQuery(e.target.value)
   }
 
-  function handleKeyDown (e: React.KeyboardEvent<HTMLInputElement>) {
-    if (e.key === 'Enter') {
-      searchNotes(query)
+  async function handleRemoveSearch () {
+    try {
+      setIsLoading(true)
+
+      setQuery('')
+
+      const notes = await NotesServices.listNotes()
+      if (notes.length > 0) {
+        setCurrentNote(notes[0])
+      }
+
+      setNotes(notes)
+    } catch {} finally {
+      setIsLoading(false)
     }
   }
 
-  function handleRemoveSearch () {
-    setQuery('')
-  }
+  async function searchNotes () {
+    try {
+      setIsLoading(true)
 
-  async function searchNotes (query: string) {
-    const response = await NotesServices.searchNote(query)
-    setNotes(response.data)
+      const temporaryNotes = notes.filter(note => (
+        note.body.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+      ))
+
+      setNotes(temporaryNotes)
+    } catch {} finally {
+      setIsLoading(false)
+    }
   }
 
   return (
-    <FormControl px={4} pt={2} id="search">
-      <FormLabel>Search</FormLabel>
-      <InputGroup>
-        <Input
-          name='sarch'
-          type='text'
-          focusBorderColor='black'
-          value={query}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-        />
-        <InputRightElement h={'full'}>
-          <Button
-            variant={'ghost'}
-            style={{ padding: 0 }}
-            onClick={handleRemoveSearch}
-          >
-            <X />
-          </Button>
-        </InputRightElement>
-      </InputGroup>
-    </FormControl>
+    <Box px={4} pt={4} pb={2} borderBottomWidth='1px'>
+      <FormControl id="search" isDisabled={isLoading}>
+        <FormLabel>Search</FormLabel>
+        <InputGroup>
+          <Input
+            name='search'
+            type='text'
+            focusBorderColor='black'
+            value={query}
+            onChange={handleChange}
+          />
+          <InputRightElement h={'full'}>
+            <Button
+              variant={'ghost'}
+              style={{ padding: 0 }}
+              onClick={handleRemoveSearch}
+            >
+              <X />
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <Button
+          disabled={query.length === 0}
+          mt={4}
+          w='full'
+          variant='ghost'
+          colorScheme='brand'
+          size='sm'
+          onClick={searchNotes}
+        >
+          Search it!
+        </Button>
+      </FormControl>
+    </Box>
   )
 }
