513a8b42543201f9272be2cacdb6fbc2ade648a7
Gabriel Lamon <lamongabriel@icloud.com>
2023-10-21 10:52:21 -0300

    updated NotesContext functions to callbackeds

--- web/src/contexts/NotesContext.tsx
+++ web/src/contexts/NotesContext.tsx
@@ -1,12 +1,14 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useCallback, useState } from 'react'
 import { Note } from '../types/note'
 
 interface NotesContextProps {
   notes: Note[]
   currentNote: Note
+  loading: boolean
 
-  setNotes: React.Dispatch<React.SetStateAction<Note[]>>
-  setCurrentNote: React.Dispatch<React.SetStateAction<Note>>
+  setNotes: (notes: Note[]) => void
+  setCurrentNote: (note: Note) => void
+  setLoading: (isLoading: boolean) => void
 }
 
 export const NotesContext = createContext<NotesContextProps>({} as NotesContextProps)
@@ -18,13 +20,28 @@ interface NotesContextProviderProps {
 export function NotesContextProvider ({ children }: NotesContextProviderProps) {
   const [notes, setNotes] = useState([] as Note[])
   const [currentNote, setCurrentNote] = useState({} as Note)
+  const [loading, setLoading] = useState(false)
+
+  const updateNotes = useCallback((notes: Note[]) => {
+    setNotes(notes)
+  }, [])
+
+  const updateCurrentNote = useCallback((note: Note) => {
+    setCurrentNote(note)
+  }, [])
+
+  const updateLoading = useCallback((isLoading: boolean) => {
+    setLoading(isLoading)
+  }, [])
 
   return (
     <NotesContext.Provider value={{
       notes,
       currentNote,
-      setNotes,
-      setCurrentNote
+      loading,
+      setNotes: updateNotes,
+      setCurrentNote: updateCurrentNote,
+      setLoading: updateLoading
     }}>
       {children}
     </NotesContext.Provider>
